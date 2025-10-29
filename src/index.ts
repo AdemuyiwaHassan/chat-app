e1c802214e15c4a8e46cd6b6bd18fe5d9ca4fa77
HassanAdemuyiwa <hassanademuyiwa68@gmail.com>
2025-10-29 17:41:56 +0100

    added js file

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,23 @@
 import express, { Request, Response } from "express";
+import { createServer } from "http";
+import { Server } from "socket.io";
 import { join } from "path";
 
 const app = express();
+const server = createServer(app);
+const io = new Server(server);
 const PORT = 3000;
 app.use(express.json());
 app.use(express.static(join(__dirname, "../public")));
+
+io.on("connection", (socket) => {
+  console.log("A user connected:", socket.id);
+});
 
 app.get("/", (req: Request, res: Response) => {
   res.send("Hello, World!!");
 });
 
-app.listen(PORT, () => {
+server.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
 });
